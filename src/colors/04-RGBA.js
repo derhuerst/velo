fb3bafceaab91b3a0f223e157c339b6dd8ac3885
Jannis R <mail@jannisr.de>
2014-09-13 12:57:02 +0200

    added unfinished `colors` module

---
+++ src/colors/04-RGBA.js
@@ -0,0 +1,38 @@
+// colors/RGBA
+// todo
+
+
+
+// Export the `RGBA` "class" and a shorthand.
+exports.RGBA = RGBA;
+exports.rgba = function (color) {
+	return new RGBA(color);
+};
+
+
+
+// Let `RGBA` inherit from `Color`.
+inherit(RGBA, Color);
+
+
+
+// todo
+function RGBA (color) {
+	// todo
+}
+
+
+
+// Add methods to the prototype of `RGBA`.
+extend(RGBA.prototype, {
+
+
+
+	// todo
+	todo: function () {
+		// todo
+	}
+
+
+
+});
